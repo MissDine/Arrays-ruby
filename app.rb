c0834b61cd97edbc95730b6d764c50a2d2874d4b
MissDine <enidnyatichi1212@gmail.com>
2022-06-07 20:21:13 +0300

    feature edit

--- app.rb
+++ app.rb
@@ -1,3 +1,10 @@
 # In ruby arras are heterogeneous hence can contain a mixture of  data types.
 array = ["Enid",1,3.0]
 # Arrays in ruby are indexed from 0 (beginning ) and -1 from the (last) 
+# How to create an array in ruby
+# new class method
+names = Array.new
+# Setting the sive of an array
+names = Array.new(20)
+data = [1,2.0, "Enid",77.9]
+puts data
